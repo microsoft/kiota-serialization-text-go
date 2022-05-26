38228e7700e2167fcb86aa01d904fdc8b805ae25
Vincent Biret <vibiret@microsoft.com>
2022-05-26 13:34:38 -0400

    - adds unit tests to ensure interface implementation

--- text_parse_node_test.go
+++ text_parse_node_test.go
@@ -1,8 +1,10 @@
 package textserialization
 
 import (
-	assert "github.com/stretchr/testify/assert"
 	testing "testing"
+
+	absser "github.com/microsoft/kiota-abstractions-go/serialization"
+	assert "github.com/stretchr/testify/assert"
 )
 
 func TestTree(t *testing.T) {
@@ -15,9 +17,17 @@ func TestTree(t *testing.T) {
 	someProp, err := parseNode.GetChildNode("someProp")
 	assert.NotNil(t, err)
 	assert.Nil(t, someProp)
-    
+
 	stringValue, err := parseNode.GetStringValue()
-    assert.Nil(t, err)
-    assert.NotNil(t, stringValue)
-    assert.Equal(t, "stringValue", *stringValue)
+	assert.Nil(t, err)
+	assert.NotNil(t, stringValue)
+	assert.Equal(t, "stringValue", *stringValue)
+}
+
+func TestTextParseNodeHonoursInterface(t *testing.T) {
+	source := "\"stringValue\""
+	sourceArray := []byte(source)
+	instance, err := NewTextParseNode(sourceArray)
+	assert.Nil(t, err)
+	assert.Implements(t, (*absser.ParseNode)(nil), instance)
 }
--- text_serialization_writer_test.go
+++ text_serialization_writer_test.go
@@ -1,12 +1,19 @@
 package textserialization
 
 import (
-	assert "github.com/stretchr/testify/assert"
 	"testing"
+
+	absser "github.com/microsoft/kiota-abstractions-go/serialization"
+	assert "github.com/stretchr/testify/assert"
 )
 
 func TestItDoesntWriteAnythingForAdditionalData(t *testing.T) {
 	serializer := NewTextSerializationWriter()
 	err := serializer.WriteAdditionalData(nil)
 	assert.NotNil(t, err)
+}
+
+func TestSerializationWriterHonoursInterface(t *testing.T) {
+	instance := NewTextSerializationWriter()
+	assert.Implements(t, (*absser.SerializationWriter)(nil), instance)
 }
